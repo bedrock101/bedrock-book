ce4ada3ce8db7c441ae3ce90f50359fb8cde572c
Jihun Lim <31366038+heuristicwave@users.noreply.github.com>
2024-11-12 14:04:28 +0900

    Update 6-2.sh

--- chapter6/6-2.sh
+++ chapter6/6-2.sh
@@ -1,5 +1,5 @@
-# CodeLlama 모델을 로컬로 복제
-git clone https://huggingface.co/codellama/CodeLlama-7b-Instruct-hf
+# Llama-3.2-3B-Instruct 모델을 로컬로 복제 
+git clone https://huggingface.co/meta-llama/Llama-3.2-3B-Instruct
 
 # 로컬에 클론 받은 파일을 S3 버킷에 업로드
-aws s3 cp ./CodeLlama-7b-Instruct-hf/ s3://{YOUR BUCKET NAME}/{PATH} --recursive
+aws s3 cp ./Llama-3.2-3B-Instruct/ s3://{YOUR BUCKET NAME}/{PATH} --recursive

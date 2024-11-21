2413ac9246478effb3e9c0acc4617023364c038b
Jihun Lim <31366038+heuristicwave@users.noreply.github.com>
2024-11-21 14:02:01 +0900

    Update and rename 4-8.py to 4-9.py

--- chapter4/4-9.py
+++ chapter4/4-9.py
@@ -1,11 +1,9 @@
 import boto3
 
-bedrock_agent_runtime = boto3.client(
-    service_name = "bedrock-agent-runtime"
-)
+bedrock_agent_runtime = boto3.client(service_name = "bedrock-agent-runtime")
 
 def retrieve(query, kbId):
-    modelArn = 'arn:aws:bedrock:us-west-2::foundation-model/anthropic.claude-v2:1'
+    modelArn = 'arn:aws:bedrock:us-west-2::foundation-model/anthropic.claude-3-sonnet-20240229-v1:0'
     
     return bedrock_agent_runtime.retrieve_and_generate(
         input={
@@ -21,7 +19,7 @@ def retrieve(query, kbId):
     )
 
 def lambda_handler(event, context):
-    response = retrieve("생애최초 특별공급은 어떻게 신청하나요?", "{KnowledgeBaseID}")
+    response = retrieve("중대재해처벌법의 대상이 누구인가요?", "{KnowledgeBaseID}")
     output = response["output"]
     citations = response["citations"]
     

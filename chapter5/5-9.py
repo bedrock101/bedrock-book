317257f3073ab5ea8acdf8bf0db95e08c116d54c
Jihun Lim <31366038+heuristicwave@users.noreply.github.com>
2024-12-23 01:47:02 +0900

    Update 5-9.py

--- chapter5/5-9.py
+++ chapter5/5-9.py
@@ -10,14 +10,21 @@ def get_article_text(url: str) -> str:
         encoded_url = url
         
     req = urllib.request.Request(encoded_url, headers={'User-Agent': 'Mozilla/5.0'})
+    
     with urllib.request.urlopen(req) as response:
         html_doc = response.read()
+    
     decoded_html = html.unescape(html_doc.decode('utf-8'))
     soup = BeautifulSoup(decoded_html, 'html.parser')
+    
     for style in soup(["style"]):
         style.decompose()
-    texts = soup.stripped_strings
-    text = '\n'.join(texts)
+        
+    # 모든 요소 내, 텍스트 추출
+    text_content = u''.join(soup.findAll(string=True))
+
+    # 연속된 공백 제거
+    text = ' '.join(text_content.split())
     
     return text
 
